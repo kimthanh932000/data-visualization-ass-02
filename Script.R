4aeb8e9ddb59841b603250f503e0cd1cea055f67
VEE\LENOVO <kimthanh932000@gmail.com>
2025-04-05 14:22:15 +0800

    2nd commit

--- Script.R
+++ Script.R
@@ -57,8 +57,32 @@ df <- na.omit(df)
 # PCA (ignore APT)
 pca.df <- prcomp(df[,c(1:8)], scale=TRUE)
 
-#Show the individual and cumulative proportion of variance
-summary(pca.df)
+
+# Q: Why data should be standardised when performing PCA?
+# A: PCA tends to give greater loadings to variables with large numeric range, leading to bias in PCA.
+#    That's why standardisation of variables is recommended.
+
+# Display and describe the individual and cumulative proportions of variance
+round(summary(pca.df)$importance, 3)
+
+# To cover at least 50% of the total variance, PC1-PC3 are needed
+# Because their cumulative proportion is 60% (0.606)
+
+# Display and interpret the coefficients for PC1, PC2 and PC3
+round(pca.df$rotation[,1:3], 3)
+
+# Key drivers:
+# PC1: 
+#     - Average.Request.Size.Bytes (0.544)
+#     - Attack.Window.Seconds (0.526)
+# PC2: 
+#     - Average.ping.to.attacking.IP.milliseconds (0.590)
+#     - Average.ping.variability (0.577)
+# PC3: 
+#     - Hits (0.542)
+#     - Average.Attacker.Payload.Entropy.Bits (0.483)
+#     - Individual.URLs.requested (0.581)
+
 
 
 # ===================================================================
