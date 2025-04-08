d7ab122ce25d40d6ed5b30379c7e595a5cef0dd0
VEE\LENOVO <kimthanh932000@gmail.com>
2025-04-08 16:25:18 +0800

    remove comments

--- Script.R
+++ Script.R
@@ -52,35 +52,16 @@ for (col in c("Average.ping.to.attacking.IP.milliseconds", "Average.ping.variabi
 df <- na.omit(df)
 
 # ===========================================================
-# (v) Perform PCA
+# (v)
 
 # PCA (ignore APT)
 pca.df <- prcomp(df[,c(1:8)], scale=TRUE)
 
-
-# Q: Why data should be standardised when performing PCA?
-# A: PCA tends to give greater loadings to variables with large numeric range, leading to bias in results.
-#    That's why standardisation of features is recommended.
-
 # Display and describe the individual and cumulative proportions of variance
 round(summary(pca.df)$importance, 3)
 
-# So, by looking at the first 3 PCs, we can say they cover at least 50% of the total variance
-# Because their cumulative proportion is approximately 60% (0.594)
-
 # Display and interpret the coefficients for PC1, PC2 and PC3
 round(pca.df$rotation[,1:3], 3)
-
-# Key drivers:
-# PC1: 
-#     - Average.Request.Size.Bytes (-0.600) => negatively correlated
-#     - Attack.Window.Seconds (-0.567) => negatively correlated
-# PC2: 
-#     - Average.ping.to.attacking.IP.milliseconds (-0.638) => negatively correlated
-#     - Average.ping.variability (-0.642) => negatively correlated
-# PC3: 
-#     - Hits (-0.558) => negatively correlated
-#     - Individual.URLs.requested (-0.583) => negatively correlated
 
 # ===================================================================
 # (vi) PCA - Biplot
@@ -98,44 +79,6 @@ fviz_pca_biplot(pca.df,
                 repel=TRUE, #Avoid label overplotting
                 addEllipses=TRUE, #Add ellipses to the plot
                 legend.title=list(colour="APT",fill="APT",alpha="APT"))
-
-# PCA PLOT:
-# **********************
-# There is significant overlap between APT and non-APT groups.
-# However, there are still some visible blue dots toward the left, so we can still see some separation along this direction.
-
-# LOADING PLOTS:
-# ***********************
-# The loading plot shows that variables such as (ARSB), (AWS), (APV), and (APTAIM) have long vectors, indicating they are more important contributors to PC1 and PC2.
-# (IUR), (ASIAC) and (Hits) => short => less contribute to the 2 PCs
-# (ARSB), (AWS), (AAPEB), (IUR) and (ASIAC) => less than 90 degree => positively correlated
-# Similarly, above 4 features => positively correlated with (APTAIM) and (APV)
-# On the other hand, (APTAIM) and (APV) are negatively correlated with (ASIAC)
-# Same thing for above 4 features with (Hits)
-# (APTAIM) and (APV) form a 90 degree with (Hits) => uncorrelated
-
-# PCA + LOADING PLOTS:
-# *************************
-# ASIAC = Attack.Source.IP.Address.Count
-# IUR = Individual.URLs.requested
-# Hits = Hits
-# APTAIM = Average.ping.to.attacking.IP.milliseconds
-# APV = Average.ping.variability
-
-# => These features don’t show a strong directionality that differentiates the two groups (APT = Yes/No).
-# => Both clusters contain similar or mixed values for these features.
-# => It's hard to tell any differences from both groups based on these features
-
-# ******************************
-# ARSB = Average.Request.Size.Bytes
-# AWS = Attack.Window.Seconds
-# AAPEB = Average.Attacker.Payload.Entropy.Bits
-# APTAIM = Average.ping.to.attacking.IP.milliseconds
-# APV = Average.ping.variability
-
-# => Pointing left, toward the region where the APT (blue) points are more concentrated.
-# => That means higher values in these features are associated more with APT activities
-
 # ===================================================================
 # (vii) 
 
@@ -156,22 +99,6 @@ ggplot(pc2.score.df,aes(x=0,y=PC2))+
   theme_minimal(base_size=14)+
   theme(legend.position = "top")+
   ylab("PC2");
-
-# => PC1 is more helpful in classifying APT vs. non-APT.
-
-# x-axis: Some separation between the red and blue points is visible, especially towards the left side.
-
-# y-axis: Red and blue points are heavily overlapped, showing no clear separation between APT and non-APT cases.
-
-# Key drivers for PC1 (absolute loading > 0.3)
-#Average.Request.Size.Bytes	(-0.544)	
-#Attack.Window.Seconds	(-0.526)	
-#Average.Attacker.Payload.Entropy.Bits	(-0.345)	
-#Average.ping.to.attacking.IP.milliseconds	(-0.358)	
-#Average.ping.variability (-0.398)
-
-
-
 
 
 
